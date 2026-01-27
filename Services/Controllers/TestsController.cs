f132d31d766ca884e91e7caef60a667a92548008
Israel Ojeda <israel.ojeda@mybrexa.com>
2026-01-27 11:34:12 -0800

    Added TestsController

---
+++ Services/Controllers/TestsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Services.Controllers
+{
+    public class TestsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Endpoint Accessible!");
+        }
+    }
+}
