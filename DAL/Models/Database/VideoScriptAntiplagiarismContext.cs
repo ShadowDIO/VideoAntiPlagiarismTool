a12aafd2379ecb0a47932a49fb73551fd80e0337
Israel Ojeda <israel.ojeda@mybrexa.com>
2026-02-11 11:55:59 -0800

    Regenerated DbContext to exclude connection string from it

--- DAL/Models/Database/VideoScriptAntiplagiarismContext.cs
+++ DAL/Models/Database/VideoScriptAntiplagiarismContext.cs
@@ -6,10 +6,6 @@ namespace DAL.Models.Database;
 
 public partial class VideoScriptAntiplagiarismContext : DbContext
 {
-    public VideoScriptAntiplagiarismContext()
-    {
-    }
-
     public VideoScriptAntiplagiarismContext(DbContextOptions<VideoScriptAntiplagiarismContext> options)
         : base(options)
     {
@@ -18,10 +14,6 @@ public partial class VideoScriptAntiplagiarismContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     public virtual DbSet<VideoInformation> VideoInformations { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=192.168.1.237;Port=3306;database=video_script_antiplagiarism;user=israel;password=Wsxcde@123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
